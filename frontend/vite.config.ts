49f698ec81d26ebb39aa1a8744327fca5e59de55
Aakash Subedi <aakash.subedi20@gmail.com>
2026-07-29 22:11:00 -0400

    modify allowedHosts to include env var for redirect URI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      tailwindcss(),
+      react(),
+    ],
+    preview: {
+      allowedHosts: [env.VITE_DISCORD_REDIRECT_URI],
+    },
+  };
 })
+
