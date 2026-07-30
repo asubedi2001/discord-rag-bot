ff63e835d75a2fb4bdb230543294bd71328305ec
Aakash Subedi <aakash.subedi20@gmail.com>
2026-07-29 22:26:16 -0400

    modify allowedHosts to allow Cloudflare Tunnel traffic

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(() => {
 
   return {
     plugins: [
@@ -12,7 +11,16 @@ export default defineConfig(({ mode }) => {
       react(),
     ],
     preview: {
-      allowedHosts: [env.VITE_DISCORD_REDIRECT_URI],
+      allowedHosts: [
+        'bot.aakash-subedi.com',
+        'aakash-subedi.com'
+      ],
+    },
+    server: {
+      allowedHosts: [
+        'bot.aakash-subedi.com',
+        'aakash-subedi.com'
+      ],
     },
   };
 })
